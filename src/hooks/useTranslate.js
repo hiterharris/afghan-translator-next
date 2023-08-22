0392d2dac69bd51745e3afd6fe70931f8ccd3c39
hiterharris <hiterharris@comcast.net>
2023-08-21 23:38:28 -0400

    add dev env, endpoint

--- src/hooks/useTranslate.js
+++ src/hooks/useTranslate.js
@@ -1,20 +1,25 @@
 import { useState, useEffect } from 'react';
 
 const useTranslate = () => {
+    const [endpoint, setEndpoint] = useState('');
     const [inputLanguage, setInputLanguage] = useState('English');
     const [loading, setLoading] = useState(false);
     const [response, setResponse] = useState('');
     const [input, setInput] = useState('');
-    const [switched, setSwitched] = useState(false)
+    const [switched, setSwitched] = useState(false);
+    const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
 
     useEffect(() => {
         setLoading(false);
         input?.length === 0 && setResponse('');
     }, [input])
 
-    const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
-    const endpoint = env === 'local' ? 'http://localhost:3001/api/translate' : 'https://afghan-translator-api.onrender.com/api/translate';
-
+    useEffect(() => {
+        if (env === 'local') setEndpoint('http://localhost:3001/api/translate')
+        else if (env === 'dev') setEndpoint('https://afghan-translator-api-dev.onrender.com/api/translate')
+        else setEndpoint('https://afghan-translator-api.onrender.com/api/translate')
+    }, [])
+    
     const translate = (input, inputLanguage) => {
         fetch(endpoint, {
             mode: 'cors',
