f9c9474f37a698ae272b376eb174adff9e67079c
hiterharris <hiterharris@comcast.net>
2023-08-25 19:18:29 -0400

    set up translate image

---
+++ src/pages/translateImage.js
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { createWorker } from "tesseract.js";
+
+function TranslateImage() {
+  const [ocr, setOcr] = useState("");
+  const [imageData, setImageData] = useState(null);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    console.log(file)
+
+    if (!file) return;
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const imageDataUri = reader.result;
+      console.log({ imageDataUri });
+      setImageData(imageDataUri);
+    };
+    reader.readAsDataURL(file);
+  }
+
+  useEffect(() => {
+    imageData !== null && (async () => {
+      const worker = await createWorker({
+        logger: m => console.log(m)
+      });
+  
+      (async () => {
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const { data: { text } } = await worker.recognize(imageData);
+        console.log(text);
+        setOcr(text);
+        await worker.terminate();
+      })();
+    })();
+  }, [imageData]);
+
+  return (
+    <div className="TranslateImage">
+      <div>
+        <p>Choose an Image</p>
+        <input
+          type="file"
+          name=""
+          id=""
+          onChange={handleImageChange}
+          accept="image/*"
+        />
+      </div>
+      <div className="display-flex">
+        <img src={imageData} alt="" srcSet="" />
+        <p style={{ margin: '5% 15%', textAlign: 'center'}}>{ocr}</p>
+      </div>
+    </div>
+  );
+}
+export default TranslateImage;
+
+// Steps:
+// 1. Capture image and return text from tessereact
+// 2. Translate text using current translate endpoint
+// 3. Return html with translated text
+// 4. Convert into pdf
+// 5. Share pdf
+// 6. Store pdf
+
+// Prompts:
+// Translate the following text to Afghan Dari and create an html document formatted and styled based on the context of the text I provide:
