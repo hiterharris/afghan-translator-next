5cfe36057a7b80cab6c0aadb2221c648b73b0854
hiterharris <hiterharris@comcast.net>
2024-07-09 11:00:41 -0400

    update env to prod

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,11 @@
+import { Capacitor } from '@capacitor/core';
 const env = process.env.NEXT_PUBLIC_ENVIRONMENT;
 const local = process.env.NEXT_PUBLIC_ENDPOINT_LOCAL;
 const dev = process.env.NEXT_PUBLIC_ENDPOINT_DEV;
 const prod = process.env.NEXT_PUBLIC_ENDPOINT_PROD;
+
+const isCapacitor = Capacitor.isNativePlatform();
+console.log('isCapacitor', isCapacitor);
 
 const apiConfig = () => {
     let endpoint = '';
