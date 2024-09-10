6ed4094b133c74741426f58d37ca89e23d29bbac
hiterharris <hiterharris@comcast.net>
2024-09-10 16:13:27 -0400

    fallback tts if using older api version

--- src/helpers/getTTS.js
+++ src/helpers/getTTS.js
@@ -2,28 +2,30 @@ import apiConfig from '@/config/apiConfig';
 
 const getTTS = async (inputLanguage, text) => {
   const { endpoint } = apiConfig();
-  
-    try {
-        const response = await fetch(`${endpoint}/tts`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({ text }),
-        });
-    
-        if (!response.ok) {
-          throw new Error('Failed to fetch audio');
-        }
 
-        const blob = await response.blob();
-        const audioUrl = URL.createObjectURL(blob);
-        const audio = new Audio(audioUrl);
+  try {
+    const response = await fetch(`${endpoint}/tts`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ text }),
+    });
 
-        audio.playbackRate = inputLanguage === 'Dari' ? 1 : 0.8;
-        audio.play();
-      } catch (error) {
-        console.error('Error fetching audio:', error);
-      }
-    return {};
+    if (!response.ok) {
+      let voice = new SpeechSynthesisUtterance(text);
+      await speechSynthesis.speak(voice); // TODO: Remove this aftere api update
+      // throw new Error('Failed to fetch audio');
+    }
+
+    const blob = await response.blob();
+    const audioUrl = URL.createObjectURL(blob);
+    const audio = new Audio(audioUrl);
+
+    audio.playbackRate = inputLanguage === 'Dari' ? 1 : 0.8;
+    audio.play();
+  } catch (error) {
+    console.error('Error fetching audio:', error);
+  }
+  return {};
 };
 
 export default getTTS;
