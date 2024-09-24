8cc63195c2c1f7cad48f464eeabd18e8ba5ebf5a
hiterharris <hiterharris@comcast.net>
2024-09-24 12:43:49 -0400

    add moesif user tracking

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import Head from 'next/head'
 import { TranslateText } from '../components';
 import Script from 'next/script';
@@ -11,17 +10,27 @@ export default function Home() {
         <meta name="description" content="Dari-English Translator App" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
-
+        <script src="//unpkg.com/moesif-browser-js@v1/moesif.min.js"></script>
+ 
       </Head>
-      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-RBF2SG2K38" />
-        <Script>
-          {`window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
+      <Script src="//unpkg.com/moesif-browser-js@v1/moesif.min.js"></Script>
+      <Script
+        id="moesif-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            moesif.init({
+              applicationId: "${process.env.NEXT_PUBLIC_MOESIF_APPLICATION_ID}",
+            });
 
-          gtag('config', 'G-RBF2SG2K38')
-          `}
-        </Script>
+            moesif.identifyUser("12345");
+
+            moesif.track("clicked_sign_up", {
+              button_label: "Get Started",
+            });
+          `,
+        }}
+      />
       <div className="App">
         <TranslateText />
       </div>
