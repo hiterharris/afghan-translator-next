fa3bb160f86167b625f2fa8c4dcb5dc2d61acdb0
hiterharris <hiterharris@comcast.net>
2024-09-27 17:49:08 -0400

    add environment to start script

--- next.config.js
+++ next.config.js
@@ -3,8 +3,13 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   images: {
-		unoptimized: true
-	}
+    unoptimized: true
+  },
+  env: {
+    NEXT_PUBLIC_ENVIRONMENT_LOCAL: process.env.NEXT_PUBLIC_ENVIRONMENT_LOCAL,
+    NEXT_PUBLIC_ENVIRONMENT_DEV: process.env.NEXT_PUBLIC_ENVIRONMENT_DEV,
+    NEXT_PUBLIC_ENVIRONMENT_PROD: process.env.NEXT_PUBLIC_ENVIRONMENT_PROD,
+  }
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
